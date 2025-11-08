bc7fe04953f6988043e8e02a1c6fe09ac05e3cca
Nabila Putri Syafrina Bukka <nabilasyafrina.business@gmail.com>
2025-11-08 14:34:17 +0800

    Initial commit

---
+++ WanderBite/WanderBite/WanderBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderBiteApp.swift
+//  WanderBite
+//
+//  Created by Nabila Putri Syafrina Bukka on 08/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WanderBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
